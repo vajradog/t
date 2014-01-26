bcd174997a9fbc364cfce70e257d5f704f0aca96
vajradog <vajradog@gmail.com>
2014-01-26 13:46:53 -0500

    Added user authentication
    Add basic user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 T::Application.routes.draw do
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
+  resources :sessions
+
   root 'static_pages#home'
   
   get "static_pages/about"
