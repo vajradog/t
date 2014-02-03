6472b6659b759b01a12f41b15d56e9fb379bc62b
vajradog <vajradog@gmail.com>
2014-02-03 10:43:25 -0500

    added s3 in model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,9 +4,16 @@ class Portfolio < ActiveRecord::Base
 
   default_scope -> { order('created_at DESC') }
   # validates :user_id, presence: true
-		has_attached_file :image, styles: {
-		portfolio: "300X300>"
-	}
+		has_attached_file :image, 
+		:storage => :s3,
+		:url => "s3_domain_url",
+		:s3_protocol => 'http',
+		:s3_credentials => {
+		:bucket => ENV['S3_BUCKET_NAME'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+
 
 end
 
