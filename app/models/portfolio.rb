cb79b8ce04cff038ace5cace71b8f1f837b1c786
vajradog <vajradog@gmail.com>
2014-04-24 09:36:17 -0400

    s3 asscociation

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,14 +3,14 @@ class Portfolio < ActiveRecord::Base
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   default_scope -> { order('created_at DESC') }
-  # validates :user_id, presence: true
-		has_attached_file :image#, 
-		# :storage => :s3,
-		# :bucket => ENV['S3_BUCKET_NAME'],
-		# :s3_credentials => {
-  #       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  #       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  # }
+    validates :user_id, presence: true
+		has_attached_file :image, 
+		 :storage => :s3,
+		 :bucket => ENV['S3_BUCKET_NAME'],
+		 :s3_credentials => {
+         :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+   }
 
 
 end
